c1e41f33ef9deb71a48dbd2b749c40673059f1cb
Dipan <Dipan>
2020-07-22 21:31:58 +0530

    comments

---
+++ js/utils.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $('#menuItems > li> a').on('click', function (e) {
+        $('#topheader .navbar-nav').find('li.active').removeClass('active');
+        $(this).parent('li').addClass('active');
+        var tpl =  $(this).data().tpl;
+        console.log(tpl);
+        loadResourse(tpl);
+    });
+    function loadResourse(tpl) {
+        $.ajax({
+            type: 'GET',
+            url: 'tpl/' + tpl + '.tpl',
+            success: function (data) {
+                $("#contentView").html(data);
+
+            }
+        });
+    };
+    loadResourse('list');
+});
